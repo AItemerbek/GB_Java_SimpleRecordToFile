86742fe4837b81ab41c838dc761ee8aeb83e7ad9
alekseytemerbek <tech.temerbek@gmail.com>
2024-02-10 22:48:36 +0300

    add errors checking

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String name = "Ivanov Ivan Ivanovich 15.01.1995 1234567 m";
+        String name = "Ivanov Ivan Ivanovich 15.01.1998 1234567 m";
         String[] newRecord = parse(name);
         switch (checkError(newRecord)) {
             case 1:
@@ -19,13 +19,21 @@ public class Main {
             case 2:
                 System.out.println("Error 2: record contains extra fields");
                 break;
+            case 3:
+                System.out.println("Error 3: incorrect date format");
+                break;
+            case 4:
+                System.out.println("Error 4: incorrect phone number");
+                break;
+            case 5:
+                System.out.println("Error 5: incorrect sex format");
+                break;
             case 0:
                 System.out.println(Arrays.toString(newRecord));
                 writeToFile(newRecord);
                 break;
             default:
         }
-
     }
 
     static String[] parse(String record) {
@@ -35,31 +43,27 @@ public class Main {
     static int checkError(String[] record) {
         if (record.length < 6) return 1;
         if (record.length > 6) return 2;
-        return 0;
-    }
-
-    static String[] createRecord(String[] record){
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
         Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}\\.\\d{4}");
         if (!pattern.matcher(record[3]).matches()) {
-            throw new RuntimeException("Incorrect date format");
+            return 3;
         }
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
             sdf.setLenient(false);
             sdf.parse(record[3]);
         } catch (ParseException e) {
-            System.out.println("Incorrect date format");;
+            return 3;
         }
         try{
             Integer.parseInt(record[4]);
         } catch (NumberFormatException e){
-            System.out.println("Incorrect phone number");
+            return 4;
         }
         if (!record[5].equals("m") && !record[5].equals("f")){
-            throw new RuntimeException("Incorrect sex");
+            return 5;
         }
-        return record;
+        return 0;
     }
 
     static void writeToFile(String[] record) throws IOException{
@@ -71,7 +75,7 @@ public class Main {
             } {
                 writer.newLine();
             }
-            System.out.println("Массив строк успешно записан в файл " + filename);
+            System.out.println("Add record to file " + filename);
         }
     }
 }
